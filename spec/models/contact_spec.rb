678fe690a44481e8ab5f5d4cbe51604c956f3ef3
tsubasa namatame <nyshk97@gmail.com>
2020-03-05 16:51:31 +0900

    contactモデルのバリデーションとテストを作成

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of :name }
+  it { is_expected.to validate_presence_of :content }
+  it { is_expected.to validate_length_of(:content).is_at_most(1000) }
 end
