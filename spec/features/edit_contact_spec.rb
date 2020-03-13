3b90836b3bd4b0caa73e947b3f57bc83945761a2
tsubasa namatame <nyshk97@gmail.com>
2020-03-13 02:55:37 +0000

    コメント追加のテスト

--- spec/features/edit_contact_spec.rb
+++ spec/features/edit_contact_spec.rb
@@ -3,15 +3,29 @@ require 'rails_helper'
 RSpec.feature "問い合わせ編集", type: :feature do
   let!(:contact) { create(:contact) }
   let(:user) { create(:user) }
+
   it '対応済に変更' do
-    login(user)
-    click_link '詳細'
-    expect(page).to have_content contact.name
+    open_contact(user)
     choose 'contact_status_2'
     click_on '更新する'
     expect(page).to have_content '更新しました'
     expect(page).to_not have_content contact.name
     click_link '対応済'
+    expect(page).to have_content contact.name
+  end
+  
+  it 'コメントを追加' do
+    comment = 'test comment'
+    open_contact(user)
+    expect(page).to_not have_content comment
+    fill_in 'comment_content', with: comment
+    click_on '追加'
+    expect(page).to have_content comment
+  end
+
+  def open_contact(user)
+    login(user)
+    click_link '詳細'
     expect(page).to have_content contact.name
   end
 end
