dae8fd0227767b26e5c4129a5ce97f4bea4e72cd
tsubasa namatame <nyshk97@gmail.com>
2020-03-13 05:35:07 +0000

    contactにuser_idを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_13_030728) do
+ActiveRecord::Schema.define(version: 2020_03_13_053044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 2020_03_13_030728) do
     t.integer "status", default: 0, comment: "0: 未対応。 1: 対応中。 2: 対応済。"
     t.string "company"
     t.string "kana"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_contacts_on_user_id"
   end
 
   create_table "tests", force: :cascade do |t|
