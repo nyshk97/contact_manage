c4ae602f97ad8b6974ba1d0e5cf8556bac3e7dca
tsubasa namatame <nyshk97@gmail.com>
2020-03-13 05:23:59 +0000

    ユーザーに名前や会社情報を追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,9 @@
 
 FactoryBot.define do
   factory :user do
+    name { '茂野吾郎' }
     email { 'test1@test.com' }
+    company_id { '1234abcd' }
     password { 'hogehoge' }
     salt { 'salt' }
     crypted_password { Sorcery::CryptoProviders::BCrypt.encrypt('hogehoge', 'salt') }
