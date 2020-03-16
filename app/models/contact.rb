f1c3591a282a1ca5ecee52df1c7701ef34c04eec
Tsubasa Namatame <tsubasanamatame@TsubasanoMacBook-Pro.local>
2020-03-16 17:03:10 +0900

    表示順を固定

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,8 +3,8 @@
 class Contact < ApplicationRecord
   has_many :comments
   belongs_to :user
-  scope :status, ->(status) { where(status: status) }
-  scope :active, -> { where.not(status: 2) }
+  scope :status, ->(status) { where(status: status).order(id: :desc) }
+  scope :active, -> { where.not(status: 2).order(id: :desc) }
   validates :name, presence: true
   validates :content, presence: true, length: { maximum: 1000 }
 end
