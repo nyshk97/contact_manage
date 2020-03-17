ffb9833cc31f7db4d8f6de4db303055dfaaab65c
TsubasaNamatame <nyshk97@gmail.com>
2020-03-17 06:03:19 +0000

    ユーザー情報編集機能を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,32 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(edit_params)
+      flash[:info] = '更新しました'
+      redirect_to user_url(@user)
+    else
+      hoge
+      render :edit
+    end
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:name, :email, :password)
+  end
+
+  def edit_params
+    params.require(:user).permit(:name, :email)
   end
 end
