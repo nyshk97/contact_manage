c34deedf79ec8743dac349778623e50562c74aff
tsubasa namatame <nyshk97@gmail.com>
2020-03-19 03:01:41 +0000

    ユーザー招待用のコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   post '/logout' => 'sessions#destroy'
   resources :comments, only: %i[create edit update destroy]
+  resources :invites, only: %i[new create edit update]
 end
